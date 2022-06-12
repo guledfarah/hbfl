6fcbd9e11906344ee92ff6dc6823b23221a78b28
gfarah7 <gfarah7@gatech.edu>
2022-06-12 17:09:56 -0400

    change 2

--- scripts/03/ec2-startup.sh
+++ scripts/03/ec2-startup.sh
@@ -7,6 +7,11 @@ git clone https://github.com/ryanmurakami/hbfl.git
 cd hbfl
 npm i
 npm run start
+cd home/ec2-user
+git clone https://github.com/guledfarah/hbfl2.git
+cd hbfl2
+npm i
+npm run start
 
 # The above commands base64 encoded for entering into UserData
 # IyEvYmluL2Jhc2gKY3VybCAtLXNpbGVudCAtLWxvY2F0aW9uIGh0dHBzOi8vcnBtLm5vZGVzb3VyY2UuY29tL3NldHVwXzE2LnggfCBzdWRvIGJhc2ggLQpzdWRvIHl1bSBpbnN0YWxsIC15IG5vZGVqcwpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdApjZCBob21lL2VjMi11c2VyCmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vcnlhbm11cmFrYW1pL2hiZmwuZ2l0CmNkIGhiZmwKbnBtIGkKbnBtIHJ1biBzdGFydA==
